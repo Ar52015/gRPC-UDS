001ba52f57b100cee9059f2feb86c21558a354b8
Ar52015 <aryan.arora2015@gmail.com>
2026-04-05 01:30:46 +0530

    feat: added mypy-protobuf type stub generation to proto compilation

--- proto_compile.sh
+++ proto_compile.sh
@@ -12,14 +12,17 @@ mkdir -p ./generated
 # add __init__.py file to generated folder
 touch ./generated/__init__.py
 
-# generate the classes and stubs using the schema
+# generate the classes, stubs, and mypy type stubs using the schema
 uv run python -m grpc_tools.protoc \
     --proto_path=proto \
     --python_out=./generated/ \
     --grpc_python_out=./generated/ \
+    --mypy_out=./generated/ \
+    --mypy_grpc_out=./generated/ \
     proto/*.proto
 
 sed -i 's/^import schema_pb2/from . import schema_pb2/' ./generated/schema_pb2_grpc.py
+sed -i 's/^import schema_pb2/from . import schema_pb2/' ./generated/schema_pb2_grpc.pyi
 
 # Verify the generated scripts
 uv run python -c "from generated import schema_pb2, schema_pb2_grpc"
